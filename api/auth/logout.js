9393daff7beb1a747a30d3631714acb21547acc1
华杰 <505967510@qq.com>
2025-07-12 17:13:31 +0800

    修复退出bug

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -2,6 +2,8 @@ import { serialize } from 'cookie';
 
 export default function handler(req, res) {
   const cookie = serialize('app_session', '', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
     maxAge: -1, // Expire the cookie immediately
     path: '/',
   });
